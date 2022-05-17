c6ea4b3d12f77491969e9b9c7976f00d03636522
Norhan_taher_mohamed <noortaher33@gmail.com>
2022-05-17 12:36:10 +0200

    edit format

--- app/Http/Controllers/Api/VINController.php
+++ app/Http/Controllers/Api/VINController.php
@@ -11,56 +11,63 @@ class VINController extends Controller
     {
         //$vin = '1GKDS13SX72119238'; //1GKDS13SX72119200 - 1GKDS13SX72119238 --  YV2AS02A1D0914807
 
-        if ($vin) {
-            $postdata = http_build_query([
-                'format' => 'json',
-                'data' => $vin
-                ]
-            );
+        if ($vin) 
+            {
+                    $postdata = http_build_query([
+                                    'format' => 'json',
+                                    'data' => $vin
+                                ]);
             
-            $opts = [
-                'http' => [
-                    'method' => 'POST',
-                    'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
-                        "Content-Length: ".strlen($postdata)."\r\n",
-                    'content' => $postdata
-                ]
-            ];
+                    $opts = [
+                        
+                        'http' => [
+                                'method' => 'POST',
+                                'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                                "Content-Length: ".strlen($postdata)."\r\n",
+                                'content' => $postdata
+                            ]
+                    ];
 
-    $apiURL = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/";
-    $context = stream_context_create($opts);
-    $fp = fopen($apiURL, 'rb', false, $context);
+                    $apiURL = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/";
+                    $context = stream_context_create($opts);
+                    $fp = fopen($apiURL, 'rb', false, $context);
 
-    $line_of_text = fgets($fp);
-    $json = json_decode($line_of_text, true);
+                    $line_of_text = fgets($fp);
+                    $json = json_decode($line_of_text, true);
 
-    fclose($fp);
+                    fclose($fp);
 
-    $html = '';
-    foreach ($json['Results'][0] as $k => $v) {
-        if($k == "Make"){
-            $html = $k . " = " . $v;
-        }
-        //$html .= '[' . $k . '] - ' . $v . '<br />';
-    }
-
-    //echo 'Vin = ' . $vin . '<br>' . $html;
-    return response()->json(['status'=>true,
-                                //'message'=>trans('app.city'),
+                    $html = '';
+                    
+                    foreach ($json['Results'][0] as $k => $v) 
+                        {
+                            if($k == "Make" && $v != NULL)
+                                {
+                                    $html = $k . " = " . $v;
+                                    return response()->json([
+                                                    'status'=>true,
+                                                    'code'=>200,
+                                                    'data'=> $html,
+                                            ],200);
+                                }
+                        }
+                    
+                    return response()->json([
+                                'status'=>false,
+                                'message'=>trans('Wrong Vin number'),
                                 'code'=>200,
-                                'data'=> $html,
                             ],200);
-    } 
-    else 
-    {
-        //echo 'No Vin Inputted';
-        return response()->json(['status'=>true,
-        'message'=>trans('No Vin Inputted'),
-        'code'=>200,
-        //'data'=>'Vin = ' . $vin . '<br>' . $html,
-        ],200);
+            } 
         
+        else 
+            {
+                return response()->json([
+                            'status'=>false,
+                            'message'=>trans('No Vin Inputted'),
+                            'code'=>200,
+                        ],200);
+        
+            }
     }
-    }
-    }
+}
 
